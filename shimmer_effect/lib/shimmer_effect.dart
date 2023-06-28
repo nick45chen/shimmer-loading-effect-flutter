997259d553d4783ed0ae4613250c6badaac08f5b
nick.chen <nick.chen@fugle.tw>
2023-06-29 00:31:01 +0800

    add: ShimmerLoading.

--- shimmer_effect/lib/shimmer_effect.dart
+++ shimmer_effect/lib/shimmer_effect.dart
@@ -1,3 +1,4 @@
 library shimmer_effect;
 
 export 'package:shimmer_effect/src/shimmer_gradient.dart';
+export 'package:shimmer_effect/src/shimmer_loading.dart';
---
+++ shimmer_effect/lib/src/shimmer_loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer_effect/src/shimmer_gradient.dart';
+
+class ShimmerLoading extends StatefulWidget {
+  const ShimmerLoading({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  final bool isLoading;
+  final Widget child;
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading> {
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) {
+      return widget.child;
+    }
+
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) => shimmerGradient.createShader(bounds),
+      child: widget.child,
+    );
+  }
+}
