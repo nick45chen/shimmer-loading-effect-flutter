0fd4cfb6e0b310850e7bc45d6a31fe344d2a2d09
nick.chen <nick.chen@fugle.tw>
2023-06-29 00:26:16 +0800

    add: shimmerGradient.

--- shimmer_effect/lib/shimmer_effect.dart
+++ shimmer_effect/lib/shimmer_effect.dart
@@ -1,7 +1,3 @@
 library shimmer_effect;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:shimmer_effect/src/shimmer_gradient.dart';
