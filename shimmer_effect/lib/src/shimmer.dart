f85b55ba086178aadb38a1f3d7c23c2a429bc6d0
nick.chen <nick.chen@fugle.tw>
2023-06-29 09:33:30 +0800

    add: method to provide access linearGradient、the size of the ShimmerState’s RenderBox.
    methods to the ShimmerState class in order to provide access to the linearGradient, the size of the ShimmerState’s RenderBox, and look up the position of a descendant within the ShimmerState’s RenderBox.

--- shimmer_effect/lib/src/shimmer.dart
+++ shimmer_effect/lib/src/shimmer.dart
@@ -19,6 +19,26 @@ class Shimmer extends StatefulWidget {
 }
 
 class ShimmerState extends State<Shimmer> {
+  Gradient get gradient => LinearGradient(
+        colors: widget.linearGradient.colors,
+        stops: widget.linearGradient.stops,
+        begin: widget.linearGradient.begin,
+        end: widget.linearGradient.end,
+      );
+
+  bool get isSized => (context.findRenderObject() as RenderBox?)?.hasSize ?? false;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  /// look up the position of a descendant within the ShimmerState’s RenderBox.
+  Offset getDescendantOffset({
+    required RenderBox descendant,
+    Offset offset = Offset.zero,
+  }) {
+    final shimmerBox = context.findRenderObject() as RenderBox;
+    return descendant.localToGlobal(offset, ancestor: shimmerBox);
+  }
+
   @override
   Widget build(BuildContext context) {
     return widget.child ?? const SizedBox();
