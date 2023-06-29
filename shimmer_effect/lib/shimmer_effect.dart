434024abb153608e94419e61bba37b7bc4af8b2c
nick.chen <nick.chen@fugle.tw>
2023-06-29 09:35:13 +0800

    add: method to provide access linearGradient、the size of the ShimmerState’s RenderBox.
    methods to the ShimmerState class in order to provide access to the linearGradient, the size of the ShimmerState’s RenderBox, and look up the position of a descendant within the ShimmerState’s RenderBox.

--- shimmer_effect/lib/shimmer_effect.dart
+++ shimmer_effect/lib/shimmer_effect.dart
@@ -2,3 +2,4 @@ library shimmer_effect;
 
 export 'package:shimmer_effect/src/shimmer_gradient.dart';
 export 'package:shimmer_effect/src/shimmer_loading.dart';
+export 'package:shimmer_effect/src/shimmer.dart';
